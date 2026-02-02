2bd92b5da90646ed16b2bff1aa7613ba7c16d1c7
Jonas Bird <jonas.bird.m@gmail.com>
2026-02-02 17:22:20 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,49 +1,43 @@
 // -*- compile-command: "node palindrome.js" -*-
-
-
-// Reverse a string:
-/*function reverse(string){
-    return Array.from(string).reverse().join("");
-}*/
+module.exports = Phrase;
 
 // controversial approach
-String.prototype.reverse = function() {
-    return Array.from(this).reverse().join("");
-}
-
-
-/*
-// Returns true for a palindrome, false otherwise.
-function palindeome(string){
-    let processedContent = string.toLowerCase();
-    return processedContent === reverse(processedContent);
-}
-*/
+String.prototype.reverse = function () {
+  return Array.from(this).reverse().join("");
+};
 
 // Defines a Phrase object.
 function Phrase(content) {
-    this.content = content;
-    // Returns content processed for palindrome testing
-    this.processedContent = function processedContent() {
+  this.content = content;
+  // Returns content processed for palindrome testing
+  this.processedContent = function processedContent() {
+    return this.letters().toLowerCase();
+  };
 
-        return this.content.toLowerCase();
-    }
-    // Returns true if the phrase is a palindrome, false otherwise
-    this.palindrome = function palindrome() {
-        return this.processedContent() === this.processedContent().reverse();
-    }
-    this.louder = function(){
-        return this.content.toUpperCase();
-    }
+  // Returns the letters in the content
+  // example: new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    const lettersRegEx = /[a-z]/gi;
+    return (this.content.match(lettersRegEx) || []).join("");
+  };
+
+  // Returns true if the phrase is a palindrome, false otherwise
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  };
+
+  this.louder = function () {
+    return this.content.toUpperCase();
+  };
 }
 
-function TranslatedPhrase(content, translation){
-    this.content = content;
-    this.translation = translation;
-    // Example of overriding a method
-    // Returns translation processed for palindrome testing.
-    this.processedContent = function processedContent (){
-        return this.processor(this.translation);
-    };
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+  // Example of overriding a method
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  };
 }
 TranslatedPhrase.prototype = new Phrase();
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Phrase object (with palindrome detector)
+
+This is a sample NPM module created in [*Learn Enough JavaScript to Be Dangerous*](https://www.learnenough.com/javascript-tutorial) by Michael Hartl.
+
+The module can be used as follows:
+
+```
+$ npm install --global jonas-bird-palindrome
+$ vim test.js
+let Phrase = require("jonas-bird-palindrome");
+let napoleonsLament = new Phrase("Able was I, ere I saw Elba.");
+console.log(napoleonsLament.palindrome());
+$ node test.js
+true
+```
+
+
